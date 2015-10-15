d8e2a579623e170a193318fd60f2477867bd5f0f
Slikrick98 <rickyeckberg@gmail.com>
2015-10-15 04:06:04 -0400

    Improved buildscript setup. Fixed mcmod.info (reverted from commit b009a5e8d9e3698d495491f47f81b7682503826b)

--- src/main/java/remoteio/common/lib/ModInfo.java
+++ src/main/java/remoteio/common/lib/ModInfo.java
@@ -3,7 +3,10 @@ package remoteio.common.lib;
 public class ModInfo {
     public static final String ID = "RIO";
     public static final String NAME = "RemoteIO";
-    public static final String VERSION = "%MOD_VERSION%";
+    public static final String MAJOR = "@MAJOR@";
+    public static final String MINOR = "@MINOR@";
+    public static final String PATCH = "@PATCH@";
+    public static final String VERSION = MAJOR + "." + MINOR + "." + PATCH;
     public static final String DEPENDENCIES = "required-after:Forge@[10.13.2.1291,);after:Waila";
 
     public static final String CLIENT = "remoteio.client.ClientProxy";
